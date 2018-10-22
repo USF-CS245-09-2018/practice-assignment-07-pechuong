204cd01ab1cd816dc75b96abb4e28caeee9b6331
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-10-22 13:13:27 -0700

    Added all the methods from List and made constructor

---
+++ ArrayList.java
@@ -0,0 +1,39 @@
+public class ArrayList implements List {
+
+	private Object[] arr;
+	private int tail;
+
+	public ArrayList() {
+		this.arr = new Object[10];
+		this.tail = 0;
+	}
+
+	public void add(Object obj) {
+
+
+	}
+
+	public void add(int pos, Object obj) {
+
+
+
+	}
+
+
+	public Object get(int pos) {
+
+
+	}
+
+	public Object remove(int pos) {
+
+
+
+	}
+
+	public int size() {
+
+
+	}
+}
+			
